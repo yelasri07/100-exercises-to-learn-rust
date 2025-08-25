8653f2590c28812ceed9ac842ba50e07ec113e03
yelasri <elasriyoussef604@gmail.com>
2025-08-25 17:55:02 +0100

    test

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -10,7 +10,6 @@ pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     //  Do you need to annotate the type of `distance`? Why or why not?
 
     // Don't change the line below
-    
     distance / time_elapsed
 }
 
